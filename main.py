3a5f4707bc5fe1c8a5e5faca2a3f4e6f3ba94d53
smcclenic <steven.mcclenic1@gmail.com>
2025-08-27 12:27:20 -0700

    get_book_text() and main() added

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("greetings boots")
+def get_book_text(file_path):
+    with open(file_path) as f:
+        return f.read()
+    
+def main():
+    book_text = get_book_text("books/frankenstein.txt")
+    print(book_text)
+
+main()

6e34815efbb919550f9ef84efb7f2b5650b1500f
smcclenic <steven.mcclenic1@gmail.com>
2025-08-28 07:56:43 -0700

    Final

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
-from stats import get_num_words
-from stats import get_book_text
+import sys
+from stats import (
+    get_num_words, 
+    get_char_dict,
+    char_dict_to_sorted
+)
     
-def main(file_path):
-    book_text = get_book_text(file_path)
-    print(book_text)
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    file_path = sys.argv[1]
 
-get_num_words("books/frankenstein.txt")
+    num_words = get_num_words(file_path)
+    char_dict = get_char_dict(file_path)
+    char_sorted = char_dict_to_sorted(char_dict)
+    print_report(file_path, num_words, char_sorted)
+
+def print_report(file_path, num_words, char_sorted):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {file_path}...")
+    print("----------- Word count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in char_sorted:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")
+
+
+main()
--- stats.py
+++ stats.py
@@ -4,4 +4,25 @@ def get_book_text(file_path):
 
 def get_num_words(file_path):
     num_words = len(get_book_text(file_path).split())
-    print(f"{num_words} words found in the document.")
+    return num_words
+
+def get_char_dict(file_path):
+    book_text = get_book_text(file_path)
+    char_dict = {}
+    for char in book_text:
+        lowered = char.lower()
+        if lowered in char_dict:
+            char_dict[lowered] += 1
+        else:
+            char_dict[lowered] = 1
+    return char_dict
+
+def sort_on(d):
+    return d["num"]
+
+def char_dict_to_sorted(char_dict):
+    char_list = []
+    for char, num in char_dict.items():
+        char_list.append({"char": char, "num": num})
+    char_list.sort(key=sort_on, reverse=True)
+    return char_list
